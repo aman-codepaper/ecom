f12badc5635c7324e1f6c721366cfaec713e84c1
aman-codepaper <aman.codepaper@gmail.com>
2024-08-03 18:23:46 +0530

    Update invoice.blade.php

--- resources/views/admin/invoice.blade.php
+++ resources/views/admin/invoice.blade.php
@@ -7,6 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>dfgd</h1>
+    <center>
+        <h3>Customer name: {{$data->name}}</h3>
+        <h3>Customer address: {{$data->rec_address}}</h3>
+        <h3>Phone : {{$data->phone}}</h3>
+        <h2>Product title: {{$data->product->title}}</h2>
+        <h2>Price: {{$data->product->price}}$</h2>
+        <img height="250" width="300" src="products/{{$data->product->image}}" alt="">
+    </center>
 </body>
 </html>
